eecfcfa7d8dfd258569cb160b3b76055c646941e
PhenAIx Prototype <riseholmerobots@lincoln.ac.uk>
2026-05-08 09:36:26 +0100

    pt update

--- yolo.py
+++ yolo.py
@@ -4,7 +4,7 @@ import numpy as np
 
 if __name__ == '__main__':
 
-    model = YOLO("yolo11n.pt")
+    model = YOLO("yolo_tomato.pt")
     train = model.train(data='Data/data.yaml', epochs=100, imgsz=640, workers=1)
     metrics = model.val()
     results = model("leaf_1.jpg")
